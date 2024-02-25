cc92d52d53676b0b0618535f74c24fec7a1c35c1
hanumasuseela <poomdlasuseela100@gmail.com>
2024-02-25 13:47:38 -0800

    SignUp Page Add

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,48 +1,34 @@
 import React, { useState } from 'react'
 import { Image, Text, TextInput, TouchableOpacity, View,StyleSheet,Button } from 'react-native'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-
+import firebase from "../firebase/config"
 
 
 export default function SignUpScreen({navigation}) {
-
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [confirmPassword, setConfirmPassword] = useState('')
+   
+   
     
-
     const onRegisterPress = () => {
       if (password !== confirmPassword) {
           alert("Passwords don't match.")
           return
       }
-  
       firebase
-          .auth()
-          .createUserWithEmailAndPassword(email, password)
-          .then((response) => {
-              const uid = response.user.uid
-              const data = {
-                  id: uid,
-                  email,
-                  fullName,
-              };
-              const usersRef = firebase.firestore().collection('users')
-              usersRef
-                  .doc(uid)
-                  .set(data)
-                  .then(() => {
-                      navigation.navigate('Home', {user: data})
-                  })
-                  .catch((error) => {
-                      alert(error)
-                  });
-          })
-          .catch((error) => {
-              alert(error)
-      });
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((response) => {
+          const uid = response.user.uid
+          const data = {
+              id: uid,
+              email,
+              lastName,
+          };
+    });
   }
 
     const styles = StyleSheet.create({
